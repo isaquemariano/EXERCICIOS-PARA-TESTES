aafa3c7fc121c4e75d6cddb10bcafe517620ae05
isaquemariano <isaquemariano.santos@gmail.com>
2021-05-13 20:26:36 -0300

    criando função rotineira

---
+++ adicionandofunçoes.py
@@ -0,0 +1,10 @@
+def soma(a, b):
+    s = a + b
+    print(s)
+
+
+# usando "def" pode-se criar qualquer função ou repetição que voce usa constantemente
+# evitando trabalho e economizando tempo.
+soma(8, 8)
+soma(150, 125)
+soma(255, 455)
